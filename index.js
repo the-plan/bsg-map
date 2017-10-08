f59d772c3aa4a352c3efea2a191537227f3a0b66
k33g <ph.charriere@gmail.com>
2017-10-08 11:18:36 +0200

    update

--- index.js
+++ index.js
@@ -6,28 +6,17 @@ const fetch = require('node-fetch');
 let port = process.env.PORT || 7070;
 
 let app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+//app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
-app.get('/raiders', (req, res) => {
-  //http://localhost:8080/api/raiders
-  fetch(`${process.env.RAIDERS_SERVICE}`, {
-    method:'GET', headers: {"Content-Type": "application/json;charset=UTF-8"}
-  })
-  .then(response => {
-    return response.json();
-  })
-  .then(jsonData => {
-    res.send({raiders: jsonData});
-  })
-  .catch(error => {
-    res.send({error: error});
-  });
+let users = ["🐼", "🐻", "🐰", "🦁"]
 
-  
-})
+app.get('/users/:id', (req, res) => {
+  let user = users[parseInt(req.params.id)]
+  res.send(user)
+});
 
 app.listen(port)
 console.log("🌍 Discovery Server is started - listening on ", port)
